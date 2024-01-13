ddfd36688ddf923f1359d7d93247dd94fc6c55da
Yakiyo <yakiyo1206@gmail.com>
2024-01-13 07:55:43 +0000

    feat: yeet everything and start from scratch

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-*/
 package main
 
-import "github.com/Yakiyo/rat/cmd"
+import "fmt"
 
 func main() {
-	cmd.Execute()
+	fmt.Println("hello")
 }
--- utils/meta/meta.go
+++
@@ -1,9 +0,0 @@
-package meta
-
-const (
-	// name of app
-	AppName = "rat"
-
-	// app version
-	Version = "0.1.0"
-)
